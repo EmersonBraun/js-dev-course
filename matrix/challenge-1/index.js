78fcc5d93360313fc081a94f49d35b24dc747043
EmersonBraun <emersonfbraun@gmail.com>
2022-06-22 20:52:30 -0300

    feat: update matrix project

---
+++ matrix/challenge-1/index.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+const FONT_SIZE = 16;
+const COLUMNS = canvas.width / FONT_SIZE;
+
+function getText() {
+    const TEXTS = [0,1,2,3,4,5,6,7,8,9]
+    return TEXTS[Math.floor(Math.random()*TEXTS.length)];
+}
+
+function getInitialColor() {
+    return 'rgba(0,0,0,0.05)'
+}
+
+function getFinalColor() {
+    return '#0F0'
+}
+
+function getFont() {
+    return FONT_SIZE + 'px arial'
+}
+
+const drops = [];
+for(let x =0; x < COLUMNS; x++){
+    drops[x] = '';
+}
+
+
+function startDrawn() {
+    document.querySelector('.container').style.display = 'none'
+
+    function draw(){
+        ctx.fillStyle = getInitialColor();
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+        ctx.fillStyle = getFinalColor();
+        ctx.font = getFont();
+        
+        for(let index=0; index < drops.length; index++){
+            const text = getText()
+            
+            ctx.fillText(text,index*FONT_SIZE,drops[index]*FONT_SIZE);
+    
+            if(drops[index] * FONT_SIZE > canvas.height || Math.random() > 0.97){
+                drops[index] = 0;
+            }
+    
+            drops[index]++;
+        }
+    }
+
+    setInterval(draw,33);
+}
--- matrix/index.js
+++ matrix/index.js
@@ -1,31 +1,49 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+const FONT_SIZE = 16;
+const COLUMNS = canvas.width / FONT_SIZE;
 
-var texts = '0123456789'.split('');
-var fontSize = 16;
-var columns = canvas.width/fontSize;
-var drops = [];
-for(var x =0; x < columns; x++){
-    drops[x] = 1;
+function getText() {
+    const TEXTS = [0,1,2,3,4,5,6,7,8,9]
+    return TEXTS[Math.floor(Math.random()*TEXTS.length)];
+}
+
+function getInitialColor() {
+    return 'rgba(0,0,0,0.05)'
+}
+
+function getFinalColor() {
+    return '#0F0'
+}
+
+function getFont() {
+    return FONT_SIZE + 'px arial'
+}
+
+const drops = [];
+for(let x =0; x < COLUMNS; x++){
+    drops[x] = '';
 }
 
 function draw(){
-    ctx.fillStyle = 'rgba(0,0,0,0.05)';
+    ctx.fillStyle = getInitialColor();
     ctx.fillRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle = '#0F0';
-    ctx.font = fontSize+ 'px arial';
-    for(var i =0; i < drops.length; i++){
-        var text = texts[Math.floor(Math.random()*texts.length)];
-        ctx.fillText(text,i*fontSize,drops[i]*fontSize);
+    ctx.fillStyle = getFinalColor();
+    ctx.font = getFont();
+    
+    for(let index=0; index < drops.length; index++){
+        const text = getText()
+        
+        ctx.fillText(text,index*FONT_SIZE,drops[index]*FONT_SIZE);
 
-        if(drops[i]*fontSize > canvas.height || Math.random() > 0.95){
-            drops[i] = 0;
+        if(drops[index] * FONT_SIZE > canvas.height || Math.random() > 0.97){
+            drops[index] = 0;
         }
 
-        drops[i]++;
+        drops[index]++;
     }
 }
 
